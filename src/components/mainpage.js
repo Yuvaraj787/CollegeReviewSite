c48a07848eca8573a939c469a79957471d118f18
Yuvaraj787 <104787096+Yuvaraj787@users.noreply.github.com>
2022-09-09 18:22:25 +0530

    Update mainpage.js

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -3,7 +3,7 @@ import Plate from "./plate";
 function Questions() {
   const [questions,setquestions] = useState([]);
   function getAll() {
-  fetch("/questions").then(
+  fetch("https://backendforcollegereviewsite.herokuapp.com/questions").then(
     x => x.json()
   ).then((y) => {
     console.log(y);
@@ -16,6 +16,7 @@ function Questions() {
   // getAll();
   return (
     <div>
+    <h1>Hi</h1>
       {questions.map((qn,ind)=>{
         return (
           <Plate key={ind} data={qn} />
