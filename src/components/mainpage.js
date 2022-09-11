d6c4d93dd3a07ee715db9aed142afef530c6f673
Yuvaraj787 <104787096+Yuvaraj787@users.noreply.github.com>
2022-09-11 11:39:42 +0530

    Added loadscreen effect

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -15,12 +15,12 @@ function Questions() {
   },[]);
   // getAll();
   return (
-    <div>
-      {questions.map((qn,ind)=>{
+    <div>   
+    {(questions.length==0)?<div className="img-box"><img src="https://media1.giphy.com/media/y1ZBcOGOOtlpC/200w.webp?cid=ecf05e47wds1c6n00n4dtpstygimby6trjwtk4od9ydr01ln&rid=200w.webp&ct=g" alt="loading" /></div>:<div>{questions.map((qn,ind)=>{
         return (
           <Plate key={ind} data={qn} />
         )
-      })}
+      })}</div>}
     </div>
   )
 }
